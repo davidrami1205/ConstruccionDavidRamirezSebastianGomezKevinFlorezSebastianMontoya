b3e816307667a4126b68e859812ab9f5ba7b4012
davidrami1205 <141851086+davidrami1205@users.noreply.github.com>
2023-11-12 15:45:26 -0500

    Delete Entregable1 directory

--- Entregable1/Entregable1/src/controladores/controladorfacturas.java
+++
@@ -1,8 +0,0 @@
-package controladores;
-
-public interface controladorfacturas {
-	public String validarcocepto(String concepto)throws Exception;
-	public double validarvalor(double valor)throws Exception;
-	public String validargenerador(String generador)throws Exception;
-	
-}
